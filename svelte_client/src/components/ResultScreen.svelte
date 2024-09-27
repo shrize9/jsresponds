4baa62af5d79e15e69a2741f48088217e5d96a98
p_kuzmin <pavel.kuzmin@indriver.com>
2024-09-27 17:10:26 +0300

    Поправлен README.md

--- svelte_client/src/components/ResultScreen.svelte
+++ svelte_client/src/components/ResultScreen.svelte
@@ -1,5 +1,6 @@
 
 <script>
+    import * as d3 from "d3"
     import {respond} from "../store/RespondStore.js";
     import {replace} from "svelte-spa-router";
     import {onDestroy, onMount} from "svelte";
@@ -7,9 +8,37 @@
     import {notifications} from "../store/notifications.js";
 
     let topPlace ="Ожидаем результат";
+    let answersPie=null;
+
+    var data = [30, 86, 168, 281, 303, 365];
 
     onMount(()=>{
         createRespondBroadcastClient().addCallback(getTopPlaceRespond);
+
+        const width=500;
+        const height = Math.min(500, width / 2);
+        const outerRadius = height / 2 - 10;
+        const innerRadius = outerRadius * 0.75;
+        const tau = 2 * Math.PI;
+        const color = d3.scaleOrdinal(d3.schemeObservable10);
+
+        const svg = d3.select(answersPie)
+            .attr("viewBox", [-width/2, -height/2, width, height]);
+
+        const arc = d3.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(outerRadius);
+
+        const pie = d3.pie().sort(null);
+
+        const path = svg.datum(data).selectAll("path")
+            .data(pie)
+            .join("path")
+            .attr("fill", (d, i) => color(i))
+            .attr("d", arc)
+            .each(function(d) { this._current = d; }); // store the initial angles
+
+        d3.select(answersPie).append(svg);
     })
     onDestroy(()=>{
         createRespondBroadcastClient().removeCallback(getTopPlaceRespond);
@@ -30,6 +59,7 @@
 </script>
 
 <div class="panel">
+    <svg width=500 height=500 bind:this={answersPie}></svg>
     <div class="panel-row">
         {topPlace}
     </div>
