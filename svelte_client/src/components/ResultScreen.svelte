ff865fe0bbeb566adf9c358f505bcb5bd91ab3df
p_kuzmin <pavel.kuzmin@indriver.com>
2024-09-28 23:02:06 +0300

    Добавлена графика

--- svelte_client/src/components/ResultScreen.svelte
+++ svelte_client/src/components/ResultScreen.svelte
@@ -9,6 +9,7 @@
 
     let topPlace ="Ожидаем результат";
     let answersPie=null;
+    let answersBar=null;
 
     var data = [
         {correct:1, askId:10, counted:20},
@@ -19,11 +20,56 @@
         {correct:1, askId:40, counted:50}
     ];
 
-    onMount(()=>{
-        createRespondBroadcastClient().addCallback(getTopPlaceRespond);
-
+    function drawBar(){
         const width=500;
         const height = Math.min(500, width / 2);
+        const margin ={
+            left:30,right:0,top:0,bottom:40
+        }
+
+        const svg = d3.select(answersBar)
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+            // Add X axis
+            var x = d3.scaleLinear()
+                .domain([0, 3])
+                .range([ 0, width]);
+
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x))
+                .selectAll("text")
+                .attr("transform", "translate(-10,0)rotate(-45)")
+                .style("text-anchor", "end");
+
+            // Y axis
+            var y = d3.scaleBand()
+                .range([ 0, height ])
+                .domain(data.map(function(d) { return d.askId; }))
+                .padding(.1);
+            svg.append("g")
+                .call(d3.axisLeft(y))
+
+            //Bars
+            svg.selectAll("myRect")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr("x", x(0) )
+                .attr("y", function(d) { return y(d.askId); })
+                .attr("width", function(d) { console.log(d); return x(d.correct+1); })
+                .attr("height", y.bandwidth() )
+                .attr("fill",function(d) { return d.correct ==0 ? "red": "green" })
+
+    }
+
+    function drawPie(){
+        const width=300;
+        const height = Math.min(300, width / 2);
         const outerRadius = height / 2 - 10;
         const innerRadius = outerRadius * 0.75;
         const tau = 2 * Math.PI;
@@ -39,7 +85,7 @@
         const pie = d3.pie().value((d)=>d.counted).sort(null);
 
         let g = svg.append("g")
-            .attr("transform", "translate(150, 120)");
+            .attr("transform", "translate(50, 110)");
 
         // Grouping different arcs
         var arcs = g.selectAll("arc")
@@ -51,11 +97,20 @@
         arcs.append("path")
             .attr("fill", (d, i) => {if(data[i].correct ==1) return color(i); else return "#FF0000"; })
             .attr("d", arc)
+            .attr("stroke-width", 5)
+            .attr("stroke", "white")
             .each(function(d) { this._current = d; });
         arcs.append("text")
             .attr("fill", d => "white")
             .attr("transform", d => `translate(${arc.centroid(d)})`)
             .text(d => `${d.data.counted}`);
+    }
+
+    onMount(()=>{
+        createRespondBroadcastClient().addCallback(getTopPlaceRespond);
+
+        drawPie();
+        drawBar();
     })
     onDestroy(()=>{
         createRespondBroadcastClient().removeCallback(getTopPlaceRespond);
@@ -76,7 +131,8 @@
 </script>
 
 <div class="panel">
-    <svg width=500 height=500 bind:this={answersPie}></svg>
+    <svg bind:this={answersPie}></svg>
+    <svg width=500 height=500 bind:this={answersBar}></svg>
     <div class="panel-row">
         {topPlace}
     </div>
